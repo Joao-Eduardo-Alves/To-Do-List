7339ec498bdbd92e0e4bd270c0337b79f3610704
Joao-Eduardo-Alves <joaoeduardo.alves@outlook.com>
2025-10-23 17:11:15 -0300

    Adicionado botão de concluir tarefa

--- frontend/src/components/ListarTarefas.jsx
+++ frontend/src/components/ListarTarefas.jsx
@@ -1,17 +1,28 @@
 import { Trash2 } from "lucide-react";
+import { CircleCheck } from "lucide-react";
 function ListarTarefas({ tarefas }) {
 
     return (
         <>
         <ul className="space-y-2">
             {tarefas.map((tarefa) => (
-                <li key={tarefa.id} className="bg-[oklch(28.2%_0.091_267.935)] text-white p-4 rounded-xl flex justify-between items-center gap-3">
-                    <span> {tarefa.descricao} </span> 
+                
+                <li key={tarefa.id} className="bg-[oklch(28.2%_0.091_267.935)] text-white p-4 rounded-xl flex items-center gap-3">
+                    
                     <button
-                        className="flex items-center bg-red-300 hover:bg-red-400 px-4 py-2 rounded-sm transition">
-                        <Trash2 size={18} color="white" />
+                        className="flex items-center bg-green-400 hover:bg-green-500 px-4 py-2 rounded-sm transition">
+                        <CircleCheck size={22} color="white" />
+                    </button> 
+                
+                    <span className="flex-1"> {tarefa.descricao} </span> 
+
+                    <button
+                        className="flex items-center bg-red-400 hover:bg-red-500 px-4 py-2 rounded-sm transition">
+                        <Trash2 size={12} color="white" />
                     </button>  
+                    
                 </li>
+
             ))}
         </ul>
         </>
